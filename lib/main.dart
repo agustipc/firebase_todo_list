5f5ca706ae17f9d798b0852acdd9676bb15270fc
agusti <apou@universaldoctor.com>
2020-04-22 10:13:30 +0200

    database

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_app/db.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,36 +19,107 @@ class MyApp extends StatelessWidget {
       home: HomePage(),
     );
   }
+
+}
+Future<String> delayed(String message) async{
+  await Future.delayed(Duration(seconds: 3));
+  return message;
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({
-    Key key,
-  }) : super(key: key);
+Stream<int> coountdown(int from) async*{
+  await Future.delayed(Duration(seconds: 3));
+  for(int i = from; i >= 0; i--){
+    yield i;
+    // yield es como un return pero sigue la funcion
+    await Future.delayed(Duration(seconds: 1));
+  }
+}
 
+class TestPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext contex){
+    return Scaffold(
+      body: StreamBuilder<int>(
+        stream: coountdown(10),
+        builder: (BuildContext context, AsyncSnapshot<int> snapshot){
+          if(snapshot.hasError){
+            return Center(child: Text('ERROR:: ${snapshot.toString()}'),);
+          }
+          switch(snapshot.connectionState){
+            case ConnectionState.waiting:
+              return Center(child: Text('Waiting...'),);
+            case ConnectionState.active:
+              return Center(child: Text('${snapshot.data}', style: TextStyle(fontSize: 40),));
+            case ConnectionState.done:
+              return Center(child: Text('Done!'),);
+            case ConnectionState.none:
+            default: 
+              return(Placeholder());
+          }
+        },
+      ),
+    );
+  }
+}
+
+//DocumentSnapshot: "Foto" de un documento en un momento de timepo
+//QuerySnapshot: "Foto" de una consulta a una coleccion
+
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Prova Firestore'
-        ),
-      ),
-      body: FutureBuilder<DocumentSnapshot>(
-        future: Firestore.instance.collection('proves').document('test1').get(),
-        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if(!snapshot.hasData){
-            return Center(child: CircularProgressIndicator());
+      appBar: AppBar(centerTitle: true ,title: Text('Firestore Todo List')),
+      body: StreamBuilder<List<Todo>>(
+        stream: todoListSnapshots(),
+        builder: (context, AsyncSnapshot<List<Todo>> snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('ERROR: ${snapshot.error.toString()}'));
           }
-          final DocumentSnapshot doc = snapshot.data;
-          Map<String, dynamic> fields = doc.data;
-          return Center(
-            child: Text(
-              fields['text']
-            ),
-          );
-        },
-      )
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.active:
+              return TodoList(todos: snapshot.data);
+            case ConnectionState.done:
+              return Center(child: Text("done??"));
+            case ConnectionState.none:
+            default:
+              return Center(child: Text("no hi ha stream??"));
+          }
+        }
+      ),
+    );
+  }
+}
+
+class TodoList extends StatelessWidget {
+  final List<Todo> todos;
+  TodoList({ @required this.todos });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: todos.length,
+      itemBuilder: (context, int index) {
+        return ListTile(
+          onTap: (){
+            Firestore.instance.collection('todos').document(todos[index].id).updateData({
+              'done': !todos[index].done
+            });
+          },
+          title: Text(todos[index].what),
+          leading: Checkbox(
+            value: todos[index].done,
+            onChanged: (newValue){
+              Firestore.instance.collection('todos').document(todos[index].id).updateData({
+                'done': newValue
+              });
+            },
+          ),
+        );
+      },
     );
   }
 }
